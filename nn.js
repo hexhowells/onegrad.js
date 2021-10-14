6a643a502b4195e27cac388b8b73eb46658004e1
Morgan <58524460+hexhowells@users.noreply.github.com>
2021-10-14 18:38:54 +0100

    added Linear layer, calls to nj will be replaced with onegrad.Tensor when implimented

---
+++ nn.js
@@ -0,0 +1,19 @@
+var numjs = require("numjs");
+
+class Linear {
+	constructor(inDim, outDim, bias=true) {
+		this.inDim = inDim;
+		this.outDim = outDim;
+		this.useBias = bias
+		this.weight = nj.random([inDim, outDim]);
+		if (this.useBias)
+			this.bias = nj.zeros([outDim])
+	}
+
+	forward(x) {
+		x = nj.dot(x, this.weight);
+		if (this.useBias)
+			x = nj.add(x, this.bias.reshape(1, -1));
+		return x;
+	}
+}
