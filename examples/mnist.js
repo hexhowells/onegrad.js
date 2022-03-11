e94f6712bea4b9a2b708583ffd250e54c7f11d2e
Morgan <58524460+hexhowells@users.noreply.github.com>
2022-03-11 17:44:29 +0000

    Add model class with Module

--- examples/mnist.js
+++ examples/mnist.js
@@ -4,20 +4,60 @@ var nn = require("./../onegrad/nn.js");
 var {loadMnist} = require("./../datasets/loadMnist")
 
 
-// model
-var layer1 = new nn.Linear(784, 200, false)
-var layer2 = new nn.Linear(200, 128, false)
-var layer3 = new nn.Linear(128, 10, false)
+// define model
+class Model extends nn.Module{
+	constructor(inDim, outDim) {
+		super()
+		this.layers = [
+			new nn.Linear(inDim, 200, false), 
+			new nn.Linear(200, 128, false), 
+			new nn.Linear(128, outDim, false)]
+	}
+
+	forward(x) {
+		x = onegrad.sigmoid(this.layers[0].forward(x))
+		x = onegrad.sigmoid(this.layers[1].forward(x))
+		x = onegrad.sigmoid(this.layers[2].forward(x))
+		return x
+	}
+}
 
 // hyperparameters
 const epochs = 10;
 var lr = 0.001;
 var batchSize = 32;
 
-var params = layer1.parameters().concat(layer2.parameters()).concat(layer3.parameters());
-var opt = new optim.Adam(params, lr=lr, bs=batchSize);
+// create objects for training
+var model = new Model(784, 10);
+var opt = new optim.Adam(model.parameters(), lr=lr, bs=batchSize);
 var lossfn = new nn.MSE();
-//var scheduler = new optim.StepLR(opt, stepSize=5, gamma=0.1)
+
+// load dataset and start training
+var [training, testing] = loadMnist(batchSize)
+console.log(`\nloaded ${training.labels.length * batchSize} images`)
+train(training, testing)
+
+
+function train(training, testing) {
+	for (let epoch=0; epoch<epochs; epoch++){
+		console.log(`\nEpoch ${epoch+1} of ${epochs}`);
+
+		for (let i=0; i<training.data.length; i++){
+			var input_tensor = training.data[i];
+			var target_tensor = training.labels[i].transpose();
+
+			var output = model.forward(input_tensor.transpose())
+
+			var loss = lossfn.compute(target_tensor, output);
+
+			loss.backward()
+			opt.step();
+			opt.zeroGrad()
+		}
+		console.log(`Accuracy: ${evaluate(training.data, training.labels)}%`)
+		console.log(`Val Accuracy: ${evaluate(testing.data, testing.labels)}%`)
+	}
+}
 
 
 function evaluate(images, labels) {
@@ -25,13 +65,12 @@ function evaluate(images, labels) {
 
 	for (let i=0; i<images.length; i++) {
 		// forward pass
-		var l1_output = onegrad.sigmoid(layer1.forward(images[i].transpose()));
-		var l2_output = onegrad.sigmoid(layer2.forward(l1_output));
-		var l3_output = onegrad.sigmoid(layer3.forward(l2_output));
-		l3_output = l3_output.transpose()
+		model.eval()
+		var output = model.forward(images[i].transpose())
+		output = output.transpose()
 		
 		// extract answer
-		var out_arr = l3_output.selection.tolist()
+		var out_arr = output.selection.tolist()
 		var label_arr = labels[i].selection.tolist()
 		for (let i=0; i<out_arr.length; i++){
 			var out_ans = out_arr[i].indexOf(Math.max(...out_arr[i]))
@@ -44,32 +83,3 @@ function evaluate(images, labels) {
 	}
 	return ((acc / (images.length*batchSize) ) * 100).toFixed(2)
 }
-
-
-
-var [training, testing] = loadMnist(batchSize)
-console.log(`\nloaded ${training.labels.length * batchSize} images`)
-
-// training
-for (let epoch=0; epoch<epochs; epoch++){
-	console.log(`\nEpoch ${epoch+1} of ${epochs}`);
-
-	for (let i=0; i<training.data.length; i++){
-		var input_tensor = training.data[i];
-		var target_tensor = training.labels[i].transpose();
-
-		var l1_output = onegrad.sigmoid(layer1.forward(input_tensor.transpose()));
-		var l2_output = onegrad.sigmoid(layer2.forward(l1_output));
-		var l3_output = onegrad.sigmoid(layer3.forward(l2_output));
-
-		var loss = lossfn.compute(target_tensor, l3_output);
-
-		loss.backward()
-		opt.step();
-		opt.zeroGrad()
-		//scheduler.step()
-	}
-	console.log(`Accuracy: ${evaluate(training.data, training.labels)}%`)
-	console.log(`Val Accuracy: ${evaluate(testing.data, testing.labels)}%`)
-}
-
